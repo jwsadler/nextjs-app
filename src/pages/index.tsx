883495ddfc84ab3434a36e36ba627e08f591fc52
James <jws@alpha-solutions.us>
2023-02-27 09:25:45 -0500

    Added ignore error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ export async function getServerSideProps() {
 }
 
 export default function Home({ data }: { data: { time: string } }) {
+  //@ts-ignore
   const serverData = JSON.parse(data);
 
   return (
